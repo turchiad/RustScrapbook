2385f3f0d4fcc2fac083fb63e4bc44c9d1c999df
turchiad <turchiad@gmail.com>
2020-12-28 19:57:58 -0500

    Changed to snake_case per Cargo's recommendation.

---
+++ hello_cargo/target/debug/HelloCargo.d
@@ -0,0 +1,1 @@
+/home/turchiad/Coding/2020/RustScrapbook/HelloCargo/target/debug/HelloCargo: /home/turchiad/Coding/2020/RustScrapbook/HelloCargo/src/main.rs
---
+++ hello_cargo/target/debug/deps/HelloCargo-c27b4e8e72ca0d7b.d
@@ -0,0 +1,5 @@
+/home/turchiad/Coding/2020/RustScrapbook/HelloCargo/target/debug/deps/HelloCargo-c27b4e8e72ca0d7b: src/main.rs
+
+/home/turchiad/Coding/2020/RustScrapbook/HelloCargo/target/debug/deps/HelloCargo-c27b4e8e72ca0d7b.d: src/main.rs
+
+src/main.rs:
