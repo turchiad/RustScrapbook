de1120f283734670935ceed0316ed287a81de237
turchiad <turchiad@gmail.com>
2021-01-01 17:34:11 -0500

    Worked with structs in 'rectangles'.

---
+++ rectangles/target/debug/deps/rectangles-01b19fe6a428ed56.d
@@ -0,0 +1,5 @@
+/home/turchiad/Coding/2020/RustScrapbook/rectangles/target/debug/deps/rectangles-01b19fe6a428ed56: src/main.rs
+
+/home/turchiad/Coding/2020/RustScrapbook/rectangles/target/debug/deps/rectangles-01b19fe6a428ed56.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/home/turchiad/Coding/2020/RustScrapbook/rectangles/target/debug/rectangles: /home/turchiad/Coding/2020/RustScrapbook/rectangles/src/main.rs
