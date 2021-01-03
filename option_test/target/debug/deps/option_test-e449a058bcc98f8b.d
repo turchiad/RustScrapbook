0f9b2570f5d723eb9d124469d4ccb00c8db0a4ab
turchiad <turchiad@gmail.com>
2021-01-03 13:04:00 -0500

    Created a small test example for working with Option<T> in Rust.

---
+++ option_test/target/debug/deps/option_test-e449a058bcc98f8b.d
@@ -0,0 +1,5 @@
+/home/turchiad/Coding/2020/RustScrapbook/option_test/target/debug/deps/option_test-e449a058bcc98f8b.rmeta: src/main.rs
+
+/home/turchiad/Coding/2020/RustScrapbook/option_test/target/debug/deps/option_test-e449a058bcc98f8b.d: src/main.rs
+
+src/main.rs:
