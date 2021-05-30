0355003d5402430426fe64dcd5fb23878794ef5e
turchiad <turchiad@gmail.com>
2021-05-29 20:09:52 -0400

    Followed up to Chapter 12.2

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,24 @@
+use std::env; // To allow access of CLI arguments
+use std::fs; // To allow for file I/O
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    //println!("{:?}", args);
+
+    // String we're looking for
+    let query = &args[1];
+
+    // Filename we're looking in
+    let filename = &args[2];
+
+    // Announce action
+    println!("Searching for {}\nIn file {}", query, filename);
+
+    // Read file
+    let contents =
+        fs::read_to_string(filename).expect("Something went wrong when reading the file.");
+
+    // Announce successful read
+    println!("With text {}", contents);
 }
