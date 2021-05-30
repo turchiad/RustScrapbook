e4f9daa09b2f7d79aa2dd68908dbea3d550c5579
turchiad <turchiad@gmail.com>
2021-05-30 11:26:23 -0400

    Refactored minigrep into main.rs and lib.rs files and created unit tests for lib.rs to ensure proper functionality

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,7 @@
 use std::env; // To allow access of CLI arguments
-use std::fs; // To allow for file I/O
 use std::process; //So the program may be terminated early
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -19,29 +20,10 @@ fn main() {
         config.query, config.filename
     );
 
-    // Read file
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong when reading the file.");
+    // Program logic
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
 
-    // Announce successful read
-    println!("With text {}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        //Check if the arguments are formatted properly
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+        process::exit(1);
     }
 }
